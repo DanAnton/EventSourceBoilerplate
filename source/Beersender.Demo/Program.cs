fd8812c1acf7d4e5efa92e931ed320b9f8b2518a
Marinel Marian <Marinel.Marian@endava.com>
2022-10-20 12:23:25 +0300

    Added Shipping label

--- source/Beersender.Demo/Program.cs
+++ source/Beersender.Demo/Program.cs
@@ -12,3 +12,10 @@ Console.WriteLine(beer1);
 
 var beer3 = beer1 with {Beer = "Classic"};
 Console.WriteLine(beer3);
+
+var sl = new Shipping_label(Shipping_provider.DHL, "DHL-123");
+var sl2 = new Shipping_label(Shipping_provider.FedEx, "FE-345");
+Console.WriteLine(sl);
+Console.WriteLine(sl.Is_valid());
+Console.WriteLine(sl2);
+Console.WriteLine(sl2.Is_valid());
---
+++ source/Beersender.Domain/Beer_package/ShippingLabel.cs
@@ -0,0 +1,19 @@
+﻿namespace Beersender.Domain.Beer_package;
+
+public enum Shipping_provider
+{
+    UPS,
+    DHL,
+    PostNL,
+    FedEx
+}
+
+public record Shipping_label(Shipping_provider Shipping_Provider,
+    string Tracking_code)
+{
+    public bool Is_valid()
+    {
+        // TODO: implemente proper checking on the label format
+        return Tracking_code.Length > 6;
+    }
+}
