c41ada34a4b2f4dcc14de2732bc9c58a1b750a53
Hannes Lowette <Hannes.Lowette@axxes.com>
2022-10-20 12:08:21 +0300

    First value type

---
+++ source/Beersender.Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Beersender.Domain.Beer_package;
+
+var beer1 = new Beer_name("Gouden Carolus", "Quadrupel Whisky Infused");
+var beer2 = new Beer_name("Gouden Carolus", "Quadrupel Whisky Infused");
+
+if(ReferenceEquals(beer1, beer2))
+    Console.WriteLine("Wait wut?");
+
+Console.WriteLine(beer1);
+
+var beer3 = beer1 with {Beer = "Classic"};
+Console.WriteLine(beer3);
---
+++ source/Beersender.Domain/Beer_package/Beer_name.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beersender.Domain.Beer_package;
+
+public record Beer_name(string Brewery, string Beer)
+{
+
+}
+
