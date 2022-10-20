85a99772b221a2ba28eb1f1c847ae66568b7c593
Hannes Lowette <Hannes.Lowette@axxes.com>
2022-10-20 12:17:29 +0300

    Shipping label

---
+++ source/Beersender.Domain/Beer_package/Shipping_label.cs
@@ -0,0 +1,20 @@
+﻿namespace Beersender.Domain.Beer_package;
+
+public enum Shipping_provider
+{
+    UPS,
+    DHL,
+    PostNL,
+    FedEx
+}
+public record Shipping_label(
+    Shipping_provider Shipping_provider,
+    string Tracking_code)
+{
+    public bool Is_valid()
+    {
+        // TODO: implement proper checking on the label format
+        return Tracking_code.Length > 6;
+    }
+}
+
